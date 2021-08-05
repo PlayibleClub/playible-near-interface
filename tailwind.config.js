24fd4d72501007004c8af3c975d7d082b1b48d52
miloshakes <miguel_dungan@dlsu.edu.ph>
2021-08-05 14:49:24 +0800

    stability update removed husky lint and jest

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
     purge: [],
     darkMode: false, // or 'media' or 'class'
     theme: {
+        fontFamily: {
+            'nunito': ['nunito', 'sans-serif'],
+            'MyFont': ['"My Font"', 'serif'] // Ensure fonts with spaces have " " surrounding it.
+        },
         extend: {}
         , colors: {
             indigo: {
