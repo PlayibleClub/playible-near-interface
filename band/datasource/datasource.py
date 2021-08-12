ac5808157f620d435eeb3765a185932ea68bcc4e
Angelo Matias <geloko@users.noreply.github.com>
2021-08-12 16:37:55 +0800

    removed prints

--- band/datasource/datasource.py
+++ band/datasource/datasource.py
@@ -6,13 +6,11 @@ URL = "http://api.stats.com/v1/stats/basketball/nba/"
 
 
 def main(path):
-    print((URL + path))
     result = requests.request(
         "GET", (URL + path)
     )
     print(result)
     json_result = result.json()
-    print(json_result)
 
     if "apiResults" in json_result:
         final_value = json_result["apiResults"]
@@ -23,7 +21,6 @@ def main(path):
 
 if __name__ == "__main__":
     try:
-        print(sys.argv)
         print(main(*sys.argv[1:]))
     except Exception as e:
         print(str(e), file=sys.stderr)
