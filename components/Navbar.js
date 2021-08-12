266752fa858fadefad1081d712c9cd41b7b11f33
Miguel Lorenzo V Dungan <miguel_dungan@dlsu.edu.ph>
2021-08-12 15:05:39 +0800

    created pack container

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,16 +4,14 @@ const Navbar = (props) => {
   const { children, color } = props;
 
   return (
-    <div data-test="Navbar" className={`bg-${color} absolute top-0 left-0 text-white-light flex flex-col w-3/6 h-full`}>
-      <Link href="/">
-        <a>HOME</a>
-      </Link>
-      <Link href="/ContractInteraction">
-        <a>PORTFOLIO</a>
-      </Link>
-      <div href="/PACKS">PACKS</div>
-      <div href="/MARKETPLACE">MARKETPLACE</div>
-      <div href="/PLAY">PLAY</div>
+    <div data-test="Navbar" className={`bg-${color} absolute top-0 left-0 text-white-light  w-3/6 h-full`}>
+      <div className="flex flex-col justify-center">
+        <Link href="/"><a>HOME</a></Link>
+        <Link href="/ContractInteraction"><a>PORTFOLIO</a></Link>
+        <Link href="/ContractInteraction"><a>PACKS</a></Link>
+        <Link href="/ContractInteraction"><a>MARKETPLACE</a></Link>
+        <Link href="/ContractInteraction"><a>PLAY</a></Link>
+      </div>
     </div>
   );
 };
