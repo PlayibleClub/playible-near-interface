ab00bf1839d535da69ed1af38a1e1637df226008
Angelo Matias <geloko@users.noreply.github.com>
2021-08-17 18:32:53 +0800

    added redux persist and logger

--- redux/store.js
+++ redux/store.js
@@ -3,12 +3,33 @@ no-underscore-dangle, no-shadow,
 import/prefer-default-export */
 
 import { createStore, applyMiddleware, compose } from 'redux';
-import ReduxThunk from 'redux-thunk';
-import { composeWithDevTools } from 'remote-redux-devtools';
+import reduxThunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import rootReducer from './reducers';
 
-export const middlewares = [ReduxThunk];
+import autoMergeLevel1 from 'redux-persist/lib/stateReconciler/autoMergeLevel1';
 
-const enhancedMiddleware = applyMiddleware(...middlewares);
+const logger = createLogger({
+    timestamps: true,
+    duration: true,
+});
 
-export const store = createStore(rootReducer, {}, composeWithDevTools(enhancedMiddleware));
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: [],
+    stateReconciler: autoMergeLevel1,
+};
+
+let middlewares = [];
+if (process.env.NODE_ENV === 'production') {
+  middlewares = [...middlewares, reduxThunk];
+} else {
+  middlewares = [...middlewares, reduxThunk, logger];
+}
+
+const pReducer = persistReducer(persistConfig, rootReducer);
+export const store = createStore(pReducer, compose(applyMiddleware(...middlewares)));
+export const persistor = persistStore(store);
