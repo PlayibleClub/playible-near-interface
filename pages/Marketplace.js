310f4e2f33fdf20a0453094f93430e378e53f35f
Miguel Lorenzo V Dungan <miguel_dungan@dlsu.edu.ph>
2021-08-19 14:02:56 +0800

    pages template created

--- pages/Marketplace.js
+++ pages/Marketplace.js
@@ -1,137 +1,202 @@
-import React, { Component, useState } from 'react'
 import { useWallet, WalletStatus } from '@terra-money/wallet-provider';
+// import Image from 'next/image';
+import * as React from 'react';
 import Header from '../components/Header';
 import Button from '../components/Button';
+import Navbar from '../components/Navbar';
 import Main from '../components/Main';
 import TitledContainer from '../components/TitledContainer';
 import RoundedContainer from '../components/RoundedContainer';
 import AthleteGrid from '../components/AthleteGrid';
+// import Roundedinput from '../components/Roundedinput';
 import AthleteContainer from '../components/AthleteContainer';
+import PerformerContainer from '../components/PerformerContainer';
+import GameResultContainer from '../components/GameResultContainer';
 import RowContainer from '../components/RowContainer';
+import HorizontalScrollContainer from '../components/HorizontalScrollContainer';
+import HorizontalContainer from '../components/HorizontalContainer';
+import PackContainer from '../components/PackContainer';
+// import fantasyLogo from '../public/fantasyinvestar.png';
+// import daily from '../public/daily.png';
+// import weekly from '../public/weekly.png';
+// import seasonal from '../public/seasonal.png';
+// import wallet from '../public/wallet.png';
 import AthleteTokenContainer from '../components/AthleteTokenContainer';
-import filterIcon from '../public/images/filter.png'
-import searchIcon from '../public/images/search.png'
 
-const playerList = [
-    {
-        name: 'STEPHEN CURRY',
-        team: 'Golden State Warriors',
-        id: '320',
-        cost: '420 UST',
-        jersey: '30',
-        positions: ['PG', 'SG'],
-        grad1: 'indigo-blue',
-        grad2: 'indigo-bluegrad',
-    },
-    {
-        name: 'LEBRON JAMES',
-        team: 'Los Angeles Lakers',
-        id: '25',
-        cost: '840 UST',
-        jersey: '23',
-        positions: ['PG', 'SG'],
-        grad1: 'indigo-purple',
-        grad2: 'indigo-purplegrad',
-    },
-    {
-        name: 'Devin Booker',
-        team: 'Phoenix Suns',
-        id: '16450',
-        cost: '21 UST',
-        jersey: '01',
-        positions: ['SF', 'C'],
-        grad1: 'indigo-darkblue',
-        grad2: 'indigo-darkbluegrad',
-    },
-    // {
-    //     name: '',
-    //     team: '',
-    //     cost: '',
-    //     jersey: '',
-    //     positions: [],
-    //     grad1: '',
-    //     grad2: '',
-    // },
-]
 
-export default function Portfolio(){
+export default function Home() {
+    const { status, connect, disconnect, availableConnectTypes } = useWallet();
 
-    const [filterMode, setMode] = React.useState(false)
+    const interactWallet = () => {
+        if (status === WalletStatus.WALLET_CONNECTED) {
+            disconnect();
+        } else {
+            connect(availableConnectTypes[1]);
+        }
+    };
+    const animals = ['Dog', 'Bird', 'Cat', 'Mouse', 'Horse'];
 
-    return(
-            <>
-                <div className="">
-                    <div className="flex flex-col w-full">
+
+
+
+    const [isClosed, setClosed] = React.useState(true)
+
+
+
+
+
+
+    return (
+        <>
+            <link href="https://fonts.googleapis.com/css?family=Nunito:400,700&display=swap" rel="stylesheet"></link>
+            <div className=" h-screen relative">
+
+
+                {isClosed ? null : <div className="flex flex-row w-full absolute z-50 top-0 left-0 ">
+                    <Navbar> </Navbar>
+                    <div className="w-2/6 h-screen" onClick={() => setClosed(true)}></div>
+                </div>}
+
+
+                <div className="flex flex-col w-full ">
                     <Header>
 
-                    <Button color="indigo-light" saturation="0" textColor="white-light" textSaturation="500" size="py-1 px-1">=</Button>
+                        <Button color="indigo-light" saturation="0" textColor="white-light" textSaturation="500" onClick={() => setClosed(false)} size="py-1 px-1">=</Button>
+
+
+
+
+
+
+
+
+
                         <div className="text-white-light">
-                        {' '}
-                        <img src="images/fantasyinvestar.png" alt="Img" />
-                    </div>
+                            {' '}
+                            <img src="images/fantasyinvestar.png" alt="Img" />
+                        </div>
+                        <Button rounded="rounded-sm " textColor="white-light" color="null" onClick={interactWallet} size="py-1 px-1">
+                            <img src="images/wallet.png" alt="Img" />
+                            {status === WalletStatus.WALLET_CONNECTED ? '*' : '+'}
+                        </Button>
+
                     </Header>
-                    </div>
+
+
+
+
+
+
+
 
                     <Main color="indigo-dark">
 
-                    <div className="flex flex-col w-full overflow-y-auto">
-                        <TitledContainer title="PORTFOLIO" className="flex w-1/2 justify-items-center">
 
-                            <div className="flex w-11/12 mb-4 mt-4">
-                                { filterMode ?
-                                    <>
-                                        <div className="rounded-md bg-indigo-light mr-1 w-12 h-11" onClick={()=>setMode(false)}>
-                                            <div className="ml-3.5 mt-4">
-                                                <img src={filterIcon}/>
-                                            </div>
-                                        </div>
 
-                                        <div className="rounded-md bg-indigo-light ml-1 h-11 w-9/12 flex">
-                                            <input className="text-xl ml-4 appearance-none bg-indigo-light focus:outline-none w-10/12" type="text" placeholder="Search..." />
-                                            <img src={searchIcon} className="object-none w-2/12"/>
-                                        </div>
-                                    </>
-                                    :
-                                    <>
-                                        <div className="rounded-md bg-indigo-light mr-1 h-11 w-9/12 flex font-thin">
-                                            <div className="text-lg ml-4 mt-2 w-9/12">
-                                                Filter by
-                                            </div>
-                                            <img src={filterIcon} className="object-none w-3/12"/>
-                                        </div>
 
-                                        <div className="rounded-md bg-indigo-light ml-1 w-12 h-11" onClick={()=>setMode(true)}>
-                                            <div className="ml-4 mt-3">
-                                                <img src={searchIcon}/>
-                                            </div>
-                                        </div>
-                                    </>
-                                }
-                            </div>
+                        <div className="flex flex-col  w-full h-full overflow-y-scroll overflow-x-hidden">
+                            <TitledContainer title="MARKETPLACE">
 
-                            <AthleteGrid>
-                                {playerList.map(function(player, i){
-                                    return (
-                                        <div className='mb-4' key={i}>
-                                            <AthleteContainer 
-                                                AthleteName={player.name} 
-                                                TeamName={player.team}
-                                                ID={player.id}
-                                                CoinValue={player.cost} 
-                                                Jersey={player.jersey} 
-                                                Positions={player.positions} 
-                                                colorgrad1={player.grad1} 
-                                                colorgrad2={player.grad2} 
-                                            />
-                                        </div>
-                                    )
-                                })}
-                            </AthleteGrid>
-                        </TitledContainer>
-                    </div>
+                                <HorizontalScrollContainer>
+                                    <HorizontalContainer> <AthleteTokenContainer AthleteName="STEPHEN CURRY" CoinValue="54" /></HorizontalContainer>
+                                    <HorizontalContainer>  <AthleteTokenContainer AthleteName="LEBRON JAMES" CoinValue="43" /></HorizontalContainer>
+                                    <HorizontalContainer>  <AthleteTokenContainer AthleteName="STEPHEN CURRY" CoinValue="54" /></HorizontalContainer>
+                                    <HorizontalContainer>  <AthleteTokenContainer AthleteName="STEPHEN CURRY" CoinValue="54" /></HorizontalContainer>
+                                </HorizontalScrollContainer>
+                            </TitledContainer>
+
+
+
+
+
+
+
+                            <TitledContainer align="justify-center" title="PLAY">
+                                <div className="pl-2 w-5/6 grid gap-x-1 gap-y-2 grid-cols-2">
+                                    <div className="rounded-md  flex justify-center"><img className="rounded-md" src="images/daily.png" alt="Italian Trulli" /></div>
+                                    <div className="rounded-md  flex justify-center"><img className="rounded-md" src="images/weekly.png" alt="Italian Trulli" /></div>
+                                    <div className="rounded-md  flex justify-center"><img className="rounded-md" src="images/seasonal.png" alt="Italian Trulli" /></div>
+
+                                </div>
+
+                            </TitledContainer>
+
+
+
+
+
+
+
+
+
+
+
+
+                            <TitledContainer className=" flex " title="GAME RESULTS">
+                                <RoundedContainer>
+                                    <select className="w-11/12 bg-indigo-light" name="games" id="cars">
+                                        <option value="All Games">All Games</option>
+                                        <option value="Some games">Some Games</option>
+                                        <option value="Games this week">Games this week</option>
+                                        <option value="Games for the month">Games for the month</option>
+                                    </select>
+                                </RoundedContainer>
+
+                                <div className="mt-2">
+                                    <RoundedContainer >
+
+                                        <ul className="w-full">
+                                            {animals.map((animal) => (
+                                                <li><GameResultContainer></GameResultContainer></li>
+
+                                            ))}
+                                        </ul>
+                                    </RoundedContainer>
+                                </div>
+
+                            </TitledContainer>
+
+
+
+
+
+
+
+
+
+                            <TitledContainer align="justify-start" className=" flex w-full justify-start" title="TOP PERFORMERS">
+                                <AthleteGrid>
+                                    <PerformerContainer AthleteName="STEPHEN CURRY" CoinValue="86.3" />
+                                    <PerformerContainer AthleteName="LEBRON JAMES" CoinValue="96.0" />
+                                    <PerformerContainer AthleteName="DEVIN BOOKER" CoinValue="76.8" />
+                                    <PerformerContainer AthleteName="ARMONI BROOKS" CoinValue="83.0" />
+                                </AthleteGrid>
+                            </TitledContainer>
+
+
+
+
+
+
+
+
+                            <TitledContainer title="PACKS">
+                                <HorizontalScrollContainer>
+                                    <HorizontalContainer> <PackContainer AthleteName="PREMIUM PACK" CoinValue="54" releaseValue="2" /></HorizontalContainer>
+                                    <HorizontalContainer>  <PackContainer AthleteName="PREMIUM PACK" CoinValue="85" releaseValue="3" /></HorizontalContainer>
+                                    <HorizontalContainer>  <PackContainer AthleteName="PREMIUM PACK" CoinValue="54" releaseValue="5" /></HorizontalContainer>
+                                    <HorizontalContainer>  <PackContainer AthleteName="PREMIUM PACK" CoinValue="54" releaseValue="10" /></HorizontalContainer>
+                                </HorizontalScrollContainer>
+                            </TitledContainer>
+
+
+                        </div>
 
                     </Main>
+
                 </div>
-            </>
-        )
+
+            </div>
+        </>
+    );
 }
