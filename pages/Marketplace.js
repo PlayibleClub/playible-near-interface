de5e650efbc99f3c304de59476d379be0160bdcb
Miguel Lorenzo V Dungan <miguel_dungan@dlsu.edu.ph>
2021-08-20 14:05:34 +0800

    marketplace template finished

--- pages/Marketplace.js
+++ pages/Marketplace.js
@@ -8,7 +8,7 @@ import Main from '../components/Main';
 import TitledContainer from '../components/TitledContainer';
 import RoundedContainer from '../components/RoundedContainer';
 import AthleteGrid from '../components/AthleteGrid';
-// import Roundedinput from '../components/Roundedinput';
+import TokenGridCol2 from '../components/TokenGridCol2';
 import AthleteContainer from '../components/AthleteContainer';
 import PerformerContainer from '../components/PerformerContainer';
 import GameResultContainer from '../components/GameResultContainer';
@@ -97,27 +97,18 @@ export default function Home() {
                         <div className="flex flex-col  w-full h-full overflow-y-scroll overflow-x-hidden">
                             <TitledContainer title="MARKETPLACE">
 
-                                <HorizontalScrollContainer>
-                                    <HorizontalContainer> <AthleteTokenContainer AthleteName="STEPHEN CURRY" CoinValue="54" /></HorizontalContainer>
-                                    <HorizontalContainer>  <AthleteTokenContainer AthleteName="LEBRON JAMES" CoinValue="43" /></HorizontalContainer>
-                                    <HorizontalContainer>  <AthleteTokenContainer AthleteName="STEPHEN CURRY" CoinValue="54" /></HorizontalContainer>
-                                    <HorizontalContainer>  <AthleteTokenContainer AthleteName="STEPHEN CURRY" CoinValue="54" /></HorizontalContainer>
-                                </HorizontalScrollContainer>
-                            </TitledContainer>
 
 
-
-
-
-
-
-                            <TitledContainer align="justify-center" title="PLAY">
-                                <div className="pl-2 w-5/6 grid gap-x-1 gap-y-2 grid-cols-2">
-                                    <div className="rounded-md  flex justify-center"><img className="rounded-md" src="images/daily.png" alt="Italian Trulli" /></div>
-                                    <div className="rounded-md  flex justify-center"><img className="rounded-md" src="images/weekly.png" alt="Italian Trulli" /></div>
-                                    <div className="rounded-md  flex justify-center"><img className="rounded-md" src="images/seasonal.png" alt="Italian Trulli" /></div>
-
-                                </div>
+                                <TokenGridCol2>
+                                    <AthleteTokenContainer AthleteName="STEPHEN CURRY" CoinValue="54" />
+                                    <AthleteTokenContainer AthleteName="STEPHEN CURRY" CoinValue="54" />
+                                    <AthleteTokenContainer AthleteName="STEPHEN CURRY" CoinValue="54" />
+                                    <AthleteTokenContainer AthleteName="STEPHEN CURRY" CoinValue="54" />
+                                    <AthleteTokenContainer AthleteName="STEPHEN CURRY" CoinValue="54" />
+                                    <AthleteTokenContainer AthleteName="STEPHEN CURRY" CoinValue="54" />
+                                    <AthleteTokenContainer AthleteName="STEPHEN CURRY" CoinValue="54" />
+                                    <AthleteTokenContainer AthleteName="STEPHEN CURRY" CoinValue="54" />
+                                </TokenGridCol2>
 
                             </TitledContainer>
 
@@ -128,66 +119,6 @@ export default function Home() {
 
 
 
-
-
-
-
-                            <TitledContainer className=" flex " title="GAME RESULTS">
-                                <RoundedContainer>
-                                    <select className="w-11/12 bg-indigo-light" name="games" id="cars">
-                                        <option value="All Games">All Games</option>
-                                        <option value="Some games">Some Games</option>
-                                        <option value="Games this week">Games this week</option>
-                                        <option value="Games for the month">Games for the month</option>
-                                    </select>
-                                </RoundedContainer>
-
-                                <div className="mt-2">
-                                    <RoundedContainer >
-
-                                        <ul className="w-full">
-                                            {animals.map((animal) => (
-                                                <li><GameResultContainer></GameResultContainer></li>
-
-                                            ))}
-                                        </ul>
-                                    </RoundedContainer>
-                                </div>
-
-                            </TitledContainer>
-
-
-
-
-
-
-
-
-
-                            <TitledContainer align="justify-start" className=" flex w-full justify-start" title="TOP PERFORMERS">
-                                <AthleteGrid>
-                                    <PerformerContainer AthleteName="STEPHEN CURRY" CoinValue="86.3" />
-                                    <PerformerContainer AthleteName="LEBRON JAMES" CoinValue="96.0" />
-                                    <PerformerContainer AthleteName="DEVIN BOOKER" CoinValue="76.8" />
-                                    <PerformerContainer AthleteName="ARMONI BROOKS" CoinValue="83.0" />
-                                </AthleteGrid>
-                            </TitledContainer>
-
-
-
-
-
-
-
-
-                            <TitledContainer title="PACKS">
-                                <HorizontalScrollContainer>
-                                    <HorizontalContainer> <PackContainer AthleteName="PREMIUM PACK" CoinValue="54" releaseValue="2" /></HorizontalContainer>
-                                    <HorizontalContainer>  <PackContainer AthleteName="PREMIUM PACK" CoinValue="85" releaseValue="3" /></HorizontalContainer>
-                                    <HorizontalContainer>  <PackContainer AthleteName="PREMIUM PACK" CoinValue="54" releaseValue="5" /></HorizontalContainer>
-                                    <HorizontalContainer>  <PackContainer AthleteName="PREMIUM PACK" CoinValue="54" releaseValue="10" /></HorizontalContainer>
-                                </HorizontalScrollContainer>
-                            </TitledContainer>
 
 
                         </div>
