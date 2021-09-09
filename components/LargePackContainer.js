59c30d83ef744456c273d9ab3a0056aa2f0bdb95
Angelo Matias <geloko@users.noreply.github.com>
2021-09-09 15:22:56 +0800

    Added link to purchase

--- components/LargePackContainer.js
+++ components/LargePackContainer.js
@@ -1,30 +1,33 @@
 import PropTypes from 'prop-types';
+import Link from 'next/link'
 
 const LargePackContainer = (props) => {
   const { children, color, imagesrc, PackName, releaseValue, CoinValue } = props;
 
   return (
-    <div data-test="LargePackContainer" className={`bg-${color}   overflow-hidden  flex flex-col  w-full  `}>
-      <div className="flex">
-        <img src={imagesrc} alt="Img" />
+    <Link href="/Purchase">
+      <div data-test="LargePackContainer" className={`bg-${color}   overflow-hidden  flex flex-col  w-full  `}>
+        <div className="flex">
+          <img src={imagesrc} alt="Img" />
 
-      </div>
-      {children}
-      <div className="flex w-full justify-center">
-        <div className=" flex flex-col w-4/5">
-          <div className="flex flex-row justify-between w-full">
-            <div className="  pb-3  ">{PackName}</div>
-            <div>{CoinValue}</div>
-          </div>
-          <div className="  text-md font-light">Release {releaseValue}</div>
-          <div className="  text-xs font-bold">
+        </div>
+        {children}
+        <div className="flex w-full justify-center">
+          <div className=" flex flex-col w-4/5">
+            <div className="flex flex-row justify-between w-full">
+              <div className="  pb-3  ">{PackName}</div>
+              <div>{CoinValue}</div>
+            </div>
+            <div className="  text-md font-light">Release {releaseValue}</div>
+            <div className="  text-xs font-bold">
 
-            {' '}
+              {' '}
 
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
