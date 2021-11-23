62d6fdbba6e4a4d79633b6f4ade0569936762337
Angelo Matias <geloko@users.noreply.github.com>
2021-11-23 16:25:56 +0800

    axios

---
+++ utils/playible/index.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+const baseURL = 'http://localhost:8000/';
+
+const axiosInstance = axios.create({
+  baseURL,
+});
+
+axiosInstance.defaults.headers['Content-Type'] = 'application/json';
+//axiosInstance.defaults.headers['Access-Control-Allow-Origin'] = '*';
+//axiosInstance.defaults.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept';
+//axiosInstance.defaults.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS, PUT, PATCH, DELETE';
+axiosInstance.defaults.withCredentials = true;
+
+axiosInstance.interceptors.request.use((request) => {
+  return request;
+});
+
+axiosInstance.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    try {
+      return error.response;
+    } catch (e) {
+      return Promise.reject(error);
+    }
+  }
+);
+
+export { axiosInstance };
