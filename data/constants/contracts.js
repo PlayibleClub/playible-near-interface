e75ccd79f2cb8abfa2efa3e50c4d8b9edc5380ef
Angelo Matias <geloko@users.noreply.github.com>
2021-12-03 16:34:43 +0800

    updated contract addresses

--- data/constants/contracts.js
+++ data/constants/contracts.js
@@ -1,3 +1,3 @@
-export const CW721 = "terra1mywdl7dxzwrhrfv2lpfcne7dzj8cfm7t67xsqa" //NFT Contract
-export const PLAYIBLE = "terra1mywdl7dxzwrhrfv2lpfcne7dzj8cfm7t67xsqa" //Playible/Fantasy Contract
-export const MARKETPLACE = "terra1mywdl7dxzwrhrfv2lpfcne7dzj8cfm7t67xsqa" //Marketplace Contract
+export const CW721 = "terra1c7sppnddt9kw4g3mcvvhm0awtvl65fvhsactup" //NFT Contract
+export const PLAYIBLE = "terra19378qfrlwr7zvzq66htytu6cxr5lmry2e89uw3" //Playible/Fantasy Contract
+export const MARKETPLACE = "terra1he8gafpra00wth0uy5scwyclhm5t7rwacvhshp" //Marketplace Contract
