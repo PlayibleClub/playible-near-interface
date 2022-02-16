03328d11258f6c3a8cbb3fd38996f985ab2c2716
Joseph Villanueva <josephgvillanueva24@gmail.com>
2022-02-17 01:42:16 +0800

    Created Mock Data to be used

---
+++ pages/PlayDetails/data/index.js
@@ -0,0 +1,94 @@
+export const playList = [
+    {
+        name: 'Game 1',
+        key: 'game1',
+        image: '/images/game.png',
+        prize: '7,484.00',
+        month:'01',
+        date:'28',
+        year:'2022',
+
+    },
+    {
+        name: 'Game 2',
+        key: 'game2',
+        image: '/images/game.png',
+        prize: '8,050.00',
+        month:'02',
+        date:'01',
+        year:'2022',
+
+    },
+    {
+        name: 'Game 3',
+        key: 'game3',
+        image: '/images/game.png',
+        prize: '4,000.00',
+        month:'02',
+        date:'05',
+        year:'2022',
+    },
+    {
+        name: 'Game 4',
+        key: 'game4',
+        image: '/images/game.png',
+        prize: '12,300.00',
+        month:'02',
+        date:'06',
+        year:'2022',
+    },
+    {
+        name: 'Game 5',
+        key: 'game5',
+        image: '/images/game.png',
+        prize: '15,050.00',
+        month:'02',
+        date:'15',
+        year:'2022',
+        
+    },
+]
+
+export const leaderBoard = [
+    {
+        wallet:'Terra1...345H',
+        score:'93.0',
+    },
+    {
+        wallet:'Terra1...4H4R',
+        score:'92.5',
+    },
+    {
+        wallet:'Terra1...343A',
+        score:'89.0',
+    },
+    {
+        wallet:'Terra1...4FGH',
+        score:'88.5',
+    },
+    {
+        wallet:'Terra1...DJ8F',
+        score:'88.2',
+    },
+    {
+        wallet:'Terra1...DFX4',
+        score:'85.0',
+    },
+    {
+        wallet:'Terra1...456D',
+        score:'84.5',
+    },
+    {
+        wallet:'Terra1...876F',
+        score:'84.0',
+    },
+    {
+        wallet:'Terra1...DFGF',
+        score:'80.0',
+    },
+    {
+        wallet:'Terra1...45ED',
+        score:'75.0',
+    },
+
+]
