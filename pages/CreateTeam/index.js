45aecab9a5fdb8efb4d1c1e060d3b647958c0a9b
Joseph Villanueva <josephgvillanueva24@gmail.com>
2022-03-09 00:39:41 +0800

    Removed unnecessary code from Create Lineup

--- pages/CreateTeam/index.js
+++ pages/CreateTeam/index.js
@@ -15,88 +15,37 @@ import Lineup from '../../pages/CreateLineup/components/Lineup.js';
 import Data from "../../data/teams.json"
 
 export default function CreateLineup() { 
-
-    const router = useRouter();
-        
+    const { query } = useRouter();
 
     return (
         <>
         <Container>
-          <div className="flex flex-col w-full overflow-y-auto h-screen justify-center self-center md:pb-12">
-                    <Main color="indigo-white">
-                        
-                        {playList.map(function(data1, i){
-                      if(router.query.id === data1.key){
-                        return(
-                        <>
-                        <div className="mt-8">
-                              <BackFunction prev={`/PlayDetails?id=${data1.key}`}/>
-                        </div>
-                          <div className="md:ml-7 flex flex-row md:flex-row">
-                              <div className='md:mr-12'>
-                                    <div className="mt-7 justify-center md:self-left md:mr-8">
-                                        <Image
-                                        src={data1.image}
-                                        width={550}
-                                        height={220}
-                                        />
+            <BackFunction prev={`/CreateTeam?id=${query.id}`}/>
+            <PortfolioContainer title="CREATE LINEUP" textcolor="text-indigo-black">
+                <div className="flex flex-col">
+            <PortfolioContainer title={`Team ${Data[0].gameId}`} textcolor="text-indigo-black"/>
+                <div className="grid grid-cols-4 gap-y-4 mt-4 md:grid-cols-4 md:ml-7 md:mt-12">
+                    {Data[0].roster[0].athletes.map(function (data, i) {
+                                return (
+                                    <div className="">
+                                        <a href={`/EnterPlayers?pos=${data.position}`+`&id=${Data[0].gameId}`}>
+                                            <div className="" key={i}>
+                                                <Lineup
+                                                    position={data.position}
+                                                    player={data.player}
+                                                    id={data.id}
+                                                    score={data.score}
+                                                    />
+                                            </div>
+                                        </a>
                                     </div>
-                              </div>
-                            <div className="flex flex-col">
-                                <div className="mt-4">
-                                    <PortfolioContainer  textcolor="indigo-black" title="GAMEPLAY"/>
-                                </div>
-                                <div className="ml-7 mt-5 font-normal">
-                                    Enter a team into the Alley-oop tournament to compete for cash prizes.
-                                </div><div className="ml-7 mt-2 font-normal">
-                                    Create a lineup by selecting five Playible Athlete Tokens now.
-                                </div>
-                            </div>
-                          </div>
-
-                          <PortfolioContainer title="CREATE LINEUP" textcolor="text-indigo-black">
-                                <div className="flex flex-col">
-                            <PortfolioContainer title={`Team ${data1.number}`} textcolor="text-indigo-black"/>
-                                <div className="grid grid-cols-4 gap-y-4 mt-4 md:grid-cols-4 md:ml-7 md:mt-12">
-                                   {Data[(data1.number)-1].roster[0].athletes.map(function (data, i) {
-                                                return (
-                                                    <div className="">
-                                                        <a href={`/EnterPlayers?pos=${data.position}`+`&id=${Data[(data1.number)-1].gameId}`}>
-                                                            <div className="" key={i}>
-                                                                <Lineup
-                                                                    position={data.position}
-                                                                    player={data.player}
-                                                                    id={data.id}
-                                                                    score={data.score}
-                                                                    />
-                                                            </div>
-                                                        </a>
-                                                    </div>
-                                                )
-                                            }
-                                        )
-                                    }
-                                </div>
-                                </div>
-                            </PortfolioContainer>
-                                </>
                                 )
                             }
-                            }
                         )
-                        }
-
-                        <div className='flex justify-center mt-8'> 
-                            <Link href="/Play">
-                                <div className="bg-indigo-buttonblue w-80 h-12 mb-16 text-center rounded-md">
-                                    <div className="mt-3 text-indigo-white font-black">
-                                        PROCEED
-                                    </div>
-                                </div>
-                            </Link>
-                        </div>
-                    </Main>
-                    </div>
+                    }
+                </div>
+                </div>
+            </PortfolioContainer>
 		</Container>
         </>
     );
