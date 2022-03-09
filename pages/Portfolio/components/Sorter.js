4ad375c8aebacc478dabe460f72ef1678c207de5
kimpfajardo <67940664+kimpfajardo@users.noreply.github.com>
2022-03-09 14:43:23 +0800

    Pagination &  Sorting for Portfolio Page

--- pages/Portfolio/components/Sorter.js
+++ pages/Portfolio/components/Sorter.js
@@ -1,42 +1,72 @@
-import React, { useState } from 'react'
-import { sortOptions } from '../data';
-
-import filterIcon from '../../../public/images/filterBlack.png'
-import searchIcon from '../../../public/images/searchBlack.png'
+import React, { useState } from 'react';
+import filterIcon from '../../../public/images/filterBlack.png';
+import searchIcon from '../../../public/images/searchBlack.png';
 
 const Sorter = (props) => {
   const {
     list, //current value of the state
     setList, //set state for the sorter/filtered list
-    setSearchText
-  } = props
+    setSearchText,
+    filterHandler,
+    filterValue
+  } = props;
+
+  const [sortMode, setSortMode] = useState(null);
+
+  const sortOptions = [
+    {
+      name: 'By Name',
+      value: 'name',
+    },
+    {
+      name: 'By Team',
+      value: 'team',
+    },
+    {
+      name: 'By Position',
+      value: 'position',
+    },
+  ];
 
   const handleSort = (mode) => {
-    const tempList = list;
-    const sortMode = sortOptions.filter((option) => option.key == mode)[0]
-    tempList.sort(sortMode.sorter)
-    setList([...tempList])
-  }
+    setSortMode(mode);
+  };
 
   return (
     <>
       <div className="flex md:mt-5 md:mr-6 invisible md:visible">
-        <div className="bg-indigo-white mr-1 h-11 w-64 flex font-thin border-2 border-indigo-lightgray border-opacity-40">
-            <select value={"default"} className="bg-indigo-white ml-3 text-lg" onChange={(e) => handleSort(e.target.value)}>
-              {sortOptions.map((option) => <option value={option.key}>{option.name}</option>)}
-            </select>
-            <img src={filterIcon} className="object-none w-1/12" />
+        <div className="bg-indigo-white mr-2 h-11 w-64 flex items-center font-thin border-2 border-indigo-lightgray border-opacity-40 relative">
+          <select
+            value={filterValue}
+            className="bg-indigo-white px-3 text-lg w-full outline-none"
+            onChange={(e) => {
+              // handleSort(e.target.value);
+              filterHandler(e.target.value);
+            }}
+          >
+            <option value={null}>All</option>
+            {sortOptions.map((option) => (
+              <option value={option.value}>{option.name}</option>
+            ))}
+          </select>
+          <img src={filterIcon} className="object-none w-1/12 absolute right-0 mr-2" />
         </div>
-          <div className="bg-indigo-white border-2 border-indigo-lightgray border-opacity-40 ml-1 h-11 w-60">
-              <div className="mt-1.5">
-                <input onChange={(e) => { setSearchText(e.target.value) }} className="text-xl text-indigo-black ml-3 appearance-none bg-indigo-white focus:outline-none w-40" placeholder="Search..." />
-                <button className="">
-                    <input type="image" src={searchIcon} className="object-none ml-8 mt-1" />
-                </button>
-              </div>
+        <div className="bg-indigo-white border-2 border-indigo-lightgray border-opacity-40 ml-1 h-11 w-60">
+          <div className="mt-1.5">
+            <input
+              onChange={(e) => {
+                setSearchText(e.target.value);
+              }}
+              className="text-xl text-indigo-black ml-3 appearance-none bg-indigo-white focus:outline-none w-40"
+              placeholder="Search..."
+            />
+            <button className="">
+              <input type="image" src={searchIcon} className="object-none ml-8 mt-1" />
+            </button>
           </div>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 export default Sorter;
