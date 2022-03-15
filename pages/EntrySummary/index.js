38503f0a9077417835f567a5e264b3ed267ef0bd
Joseph Villanueva <josephgvillanueva24@gmail.com>
2022-03-15 14:14:33 +0800

    Added Edit Team Name Modal

--- pages/EntrySummary/index.js
+++ pages/EntrySummary/index.js
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import React, { useState } from 'react';
 import Main from '../../components/Main';
 import PortfolioContainer from '../../components/containers/PortfolioContainer';
+import ModalPortfolioContainer from '../../components/containers/ModalPortfolioContainer';
 import Container from '../../components/containers/Container';
 import BackFunction from '../../components/buttons/BackFunction';
 
@@ -18,32 +19,54 @@ import PlayDetailsComponent from '../../pages/PlayDetails/components/PlayDetails
 export default function EntrySummary() { 
     const { query } = useRouter();
     const router = useRouter();
+    const [name, setName] = useState("");
 
-    // const data5 = JSON.parse(Data);
+    const [changeNameModal, setchangeNameModal] = useState(false);
 
-    console.log(query)
-        
-    async function createGameData(){
-
-        if(!router.query.id) 
-        {
-            return
-        }
-
-        const response = await fetch('/api/team/',
-        {method:'POST',
-        headers:{
-            'Content-Type':'application/json'
-        },
-        body:JSON.stringify({gameId:router.query.id})
-    })
-    const res = await response.json()
-    console.log(res)
-
+    function reset (){
+        setName("")
+        return
     }
+
+        
+    // const data5 = JSON.parse(Data);
 
     return (
         <>
+        { changeNameModal === true &&
+        <>
+        <div className="fixed w-screen h-screen bg-opacity-70 z-50 overflow-auto bg-indigo-gray flex font-montserrat">
+            <div className="relative p-8 bg-indigo-white w-11/12 md:w-4/12 h-10/12 md:h-auto m-auto flex-col flex rounded-lg">
+                <button onClick={()=>{setchangeNameModal(false)}}>
+                    <div className="absolute top-0 right-0 p-4 font-black">
+                        X
+                    </div>
+                </button>
+                    <ModalPortfolioContainer  textcolor="indigo-black" title="EDIT TEAM NAME"/>
+                <div className='ml-7'>
+                    <div>
+                       Enter Team Name 
+                    </div>
+                    <div className='flex border justify-between p-4 text-bold font-monument'>
+                    <form>
+                            <input 
+                            type="text" 
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        /> 
+                    </form>
+                    <button className='mr-4' onClick={reset}>
+                        X
+                    </button>
+                    </div>
+                    <button className='bg-indigo-buttonblue w-full h-12 text-center font-bold rounded-md text-sm mt-4 self-center text-indigo-white' onClick={()=>{setchangeNameModal(false)}}>
+                            PROCEED
+                        </button>
+                </div>
+            </div>
+        </div>
+        </>
+        }
         <Container>
           <div className="flex flex-col w-full overflow-y-auto h-screen justify-center self-center md:pb-12">
                     <Main color="indigo-white">
@@ -96,8 +119,12 @@ export default function EntrySummary() {
                                     </div>
                               </div>
                               </div>
-                              <div className='mt-4'>
+                              <div className='mt-4 flex'>
                                 <PortfolioContainer title={`Team ${query.number}`} textcolor="text-indigo-black" className/>
+                                
+                                <button className='flex mt-12 ml-4 text-sm underline' onClick={()=>setchangeNameModal(true)}>
+                                    Edit team name
+                                </button>
                               </div>
                               <div className="grid grid-cols-4 gap-y-4 mt-4 md:grid-cols-4 md:ml-7 md:mt-12">
                                  {Data[0].roster[(query.number)-1].athletes.map(function (data, i) {
