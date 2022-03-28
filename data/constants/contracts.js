b0083817837faba0d2a6797dd59035ef27f2f9cd
Joseph Villanueva <josephgvillanueva24@gmail.com>
2022-03-28 16:13:16 +0800

    Updated Contracts

--- data/constants/contracts.js
+++ data/constants/contracts.js
@@ -1,8 +1,8 @@
 export const CW721 = 'terra1c7sppnddt9kw4g3mcvvhm0awtvl65fvhsactup'; //NFT Contract
 export const PLAYIBLE = 'terra19378qfrlwr7zvzq66htytu6cxr5lmry2e89uw3'; //Playible/Fantasy Contract
 export const MARKETPLACE = 'terra1he8gafpra00wth0uy5scwyclhm5t7rwacvhshp'; //Marketplace Contract
-export const ATHLETE = 'terra13jaqt6cu09qxx2f7cqlgr2eadel4kcu8dlny5u'; //Athlete Contract
-export const PACK = 'terra1ew8jm7pev4v9q9u3pkc4c7zkp78jqlesk3xym3'; //Pack Contract
+export const ATHLETE = 'terra17uqelpwpq2fm5ql7jzk2y06medmzl040y06er9'; //Athlete Contract
+export const PACK = 'terra1ja00hdxr9f0gn4l77vrv3uck3h2ev4tpvtn2ux'; //Pack Contract
 export const OPENPACK = 'terra1kn9fe70ncsterdc5ksq9tcgjuypl748cdqhnuz'; //OpenPack Contract
 export const ORACLE = 'terra1lklauqwwh4ap9r3xfvnqdrjav0gx8z7pf6c7dt'; //Oracle Contract
 export const CONTROLLER = 'terra1w0t73qkkj50zdq5fkqcedm3ewwz2smm9g8hwns'; // Controller Contract
