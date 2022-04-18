9d222dffe4654ad00da72f1a26d0050d4badd2d1
Joseph Villanueva <josephgvillanueva24@gmail.com>
2022-04-18 14:43:04 +0800

    Fixed routing

--- pages/Portfolio/index.js
+++ pages/Portfolio/index.js
@@ -262,22 +262,24 @@ const Portfolio = () => {
                           <div className="grid grid-cols-2 md:grid-cols-4 mt-12">
                             {sortedList.map(function (player, i) {
                               const path = player.token_info.info.extension;
+                              {console.log(player)}
                               return (
                                 <Link
                                   href={{
                                     pathname: '/AssetDetails',
                                     query: {
-                                      id: path.athlete_id,
+                                      id: path.attributes[0].value,
                                       origin: 'Portfolio',
                                       token_id: player.token_id,
                                     },
                                   }}
                                 >
+
                                   <div className="mb-4" key={i}>
                                     <PerformerContainer
                                       AthleteName={path.name}
                                       AvgScore={player.fantasy_score}
-                                      id={path.athlete_id}
+                                      id={path.attributes[0].value}
                                       uri={
                                         player.nft_image || player.token_info
                                           ? player.token_info.info.token_uri
