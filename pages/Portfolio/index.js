e5e2acc7e2ad3bc1e0c6a78965a919fd4f6daf98
kimpfajardo <67940664+kimpfajardo@users.noreply.github.com>
2022-04-19 16:11:14 +0800

    Update index.js

--- pages/Portfolio/index.js
+++ pages/Portfolio/index.js
@@ -206,8 +206,6 @@ const Portfolio = () => {
       setWallet(null);
       setLoading(false);
     }
-
-    console.log('connectedWallet', connectedWallet);
   }, [dispatch, connectedWallet]);
 
   useEffect(() => {
