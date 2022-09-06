b395886960b7e5fc676dcebae473f97b386fcfe5
Lilith <nekopass.420loli@gmail.com>
2022-09-07 05:02:36 +0800

    updated, only loads packs within contract offset

--- pages/Packs/index.tsx
+++ pages/Packs/index.tsx
@@ -8,11 +8,11 @@ import BackFunction from '../../components/buttons/BackFunction';
 import Main from '../../components/Main';
 import 'regenerator-runtime/runtime';
 import Link from 'next/link';
+import PackComponent from './components/PackComponent';
 import PlayComponent from '../Play/components/PlayComponent';
 import { useWalletSelector } from 'contexts/WalletSelectorContext';
 import { getRPCProvider, getContract } from 'utils/near';
 import { PACK } from '../../data/constants/nearContracts';
-import PackContainer from './components/PackContainer';
 import ReactPaginate from 'react-paginate';
 
 export default function Packs() {
@@ -31,33 +31,79 @@ export default function Packs() {
   const [filterMode, setMode] = useState(false);
   const [showFilter, setFilter] = useState(false);
   const [packs, setPacks] = useState([]);
-  const [currentPacks, setCurrentPacks] = useState(null);
+
   const [pageCount, setPageCount] = useState(0);
   const [packOffset, setPackOffset] = useState(0);
+  const [packLimit, setPackLimit] = useState(30);
+  const [totalPacks, setTotalPacks] = useState(0);
 
-  function query_nft_tokens_for_owner() {
-    const query = JSON.stringify({ account_id: accountId, limit: 15 });
+  function query_nft_supply_for_owner() {
+    const query = JSON.stringify({ account_id: accountId });
 
     provider
       .query({
         request_type: 'call_function',
         finality: 'optimistic',
         account_id: getContract(PACK),
-        from_index: 2,
+        method_name: 'nft_supply_for_owner',
+        args_base64: Buffer.from(query).toString('base64'),
+      })
+      .then((data) => {
+        // @ts-ignore:next-line
+        const totalPacks = JSON.parse(Buffer.from(data.result));
+
+        setTotalPacks(totalPacks);
+      });
+  }
+
+  function getPackLimit() {
+    try {
+      if (totalPacks>30) {
+        const _packLimit = 15;
+        console.log("Reloading packs")
+        setPackLimit(_packLimit);
+      } 
+    } catch (e) {
+      setPackLimit(30);
+    }
+  }
+
+  useEffect(() => {
+    query_nft_supply_for_owner();
+    getPackLimit();
+    setPageCount(Math.ceil(totalPacks / packLimit));
+  }, [totalPacks, packLimit])
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * packLimit) % totalPacks;
+    setPackOffset(newOffset);
+  }
+
+  useEffect(() => {
+    setPageCount(Math.ceil(totalPacks / packLimit));
+    const endOffset = packOffset + packLimit;
+    console.log(`Loading packs from ${packOffset} to ${endOffset}`);
+    query_nft_tokens_for_owner();
+  }, [packOffset, packLimit]);
+
+  function query_nft_tokens_for_owner() {
+    const query = JSON.stringify({ account_id: accountId, from_index: packOffset.toString(), limit: packLimit });
+
+    provider
+      .query({
+        request_type: 'call_function',
+        finality: 'optimistic',
+        account_id: getContract(PACK),
         method_name: 'nft_tokens_for_owner',
         args_base64: Buffer.from(query).toString('base64'),
       })
       .then((data) => {
         // @ts-ignore:next-line
         const result = JSON.parse(Buffer.from(data.result).toString());
-        // Save minter config into state
+
         setPacks(result);
       });
   }
-
-  useEffect(() => {
-    query_nft_tokens_for_owner();
-  }, []);
 
   const onSubmit = (data) => {
     if (data.search) setResult(data.search);
@@ -89,46 +135,21 @@ export default function Packs() {
 
   // if (isNarrowScreen) {
 
-  //ShowPacks includes pagination
-  //Function to show all packs is in PackContainer 
-  function ShowPacks({ packsPerPage }) {
-
-    const [currentPacks, setCurrentPacks] = useState(null);
-    const [pageCount, setPageCount] = useState(0);
-    const [packOffset, setPackOffset] = useState(0);
-
-    useEffect(() => {
-      const endOffset = packOffset + packsPerPage;
-      console.log(`ShowPacks: Loading items from ${packOffset} to ${endOffset}`);
-      setCurrentPacks(packs.slice(packOffset, endOffset));
-      setPageCount(Math.ceil(packs.length / packsPerPage));
-    }, [packOffset, packsPerPage]);
-
-    const handlePageClick = (event) => {
-      const newOffset = (event.selected * packsPerPage) % packs.length;
-      console.log(`ShowPacks: Requested page number ${event.selected}, which is offset ${newOffset}`);
-      setPackOffset(newOffset);
-    }
-
-    if(pageCount == 1) {
-      return (
-        <>
-        <PortfolioContainer textcolor="indigo-black" title="PACKS">
-          <div className="flex flex-col">
-          <PackContainer accountPacks={currentPacks}></PackContainer>
-          </div>
-        </PortfolioContainer>
-      </>
-      )}
-    else 
-    return (
-      <>
-        <PortfolioContainer textcolor="indigo-black" title="PACKS">
-          <div className="flex flex-col">
-          <PackContainer accountPacks={currentPacks}></PackContainer>
-          </div>
-        </PortfolioContainer>
-        <div className="absolute bottom-10 right-10">
+  return (
+    <Container activeName="SQUAD">
+      <div className="flex flex-col w-full overflow-y-auto h-screen pb-12 mb-12">
+        <Main color="indigo-white">
+          <div className="md:ml-6">
+            <PortfolioContainer textcolor="indigo-black" title="PACKS">
+              <div className="flex flex-col">
+              <div className="grid grid-cols-4 gap-y-8 mt-4 md:grid-cols-4 md:ml-7 md:mt-12">
+                  {packs.map(({ metadata, token_id }) => (
+                    <PackComponent image={metadata.media} id={token_id}></PackComponent>
+                  ))}
+                </div>
+              </div>
+            </PortfolioContainer>
+            <div className="absolute bottom-10 right-10">
           <ReactPaginate 
             className="p-2 bg-indigo-buttonblue text-indigo-white flex flex-row space-x-4 select-none ml-7"
             pageClassName="hover:font-bold"
@@ -142,36 +163,10 @@ export default function Packs() {
             previousLabel="< prev"
             renderOnZeroPageCount={null}
           />
-        </div>
-      </>
-    )};
-
-    const packCounter = (packAmount) => {
-      let packCount = 0;
-
-      // if(packAmount > 30) {
-      //   return packCount = 15;
-      // }
-      // else {
-      //   return packCount = 30;
-      // }
-      if(packAmount > 10) {
-        return packCount = 5;
-      }
-      else {
-        return packCount = 30;
-      }
-    }
-
-    return (
-      <Container activeName="SQUAD">
-        <div className="flex flex-col w-full overflow-y-auto h-screen pb-12 mb-12">
-          <Main color="indigo-white">
-            <div className="md:ml-6">
-              <ShowPacks packsPerPage={packCounter(packs.length)}></ShowPacks>
-            </div>
-          </Main>
-        </div>
-      </Container>
-    )
-};
+          </div>
+          </div>
+        </Main>
+      </div>
+    </Container>
+  );
+}
