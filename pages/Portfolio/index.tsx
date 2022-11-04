7c70615767e69469a833ffe319fa3f5b22ae7f85
Ramon Arca <ramonteodoroarca@gmail.com>
2022-11-04 14:06:44 +0800

    Add placeholder team dropdown and name searchbox
    For filtering test purposes only, will change to much better UI

--- pages/Portfolio/index.tsx
+++ pages/Portfolio/index.tsx
@@ -258,8 +258,45 @@ const Portfolio = () => {
                         </option>
                       </select>
                     </form>
+                    {/* <form>
+                      <select onChange={(e) => console.log(e)} className="filter-select bg-white">
+                        <option value="allTeams">
+                          ALL
+                        </option>
+                        <option value="TEN">
+                          Tennessee
+                        </option>
+                      </select>
+                    </form> */}
+
+                    
+
                     <img src={filterIcon} className="object-none w-4 mr-4" />
-              </div>
+                </div>
+                <div className="bg-indigo-white h-8 flex justify-between center w-72 mt-3 font-thin border-2 border-indigo-lightgray border-opacity-50 ml-3">
+                    <form>
+                      <select onChange={(e) => console.log(e)} className="filter-select bg-white">
+                        <option value="allTeams">
+                          ALL
+                        </option>
+                        <option value="TEN">
+                          Tennessee
+                        </option>
+                      </select>
+                    </form>
+                </div>
+                <div className="bg-indigo-white pl-8 h-8 flex justify-between center mt-3 w-96">
+                    <form>   
+                        <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Search</label>
+                        <div className="relative">
+                            <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                                <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                            </div>
+                            <input type="search" id="default-search" className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder=""/>
+                            <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+                        </div>
+                    </form>
+                </div>
               </div>
 
           <div className="md:ml-6">
