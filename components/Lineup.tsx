03437427355e6b7eabec6bd09061b3d04e151d40
Seaver Choy <matthew.seaver.choy@gmail.com>
2022-11-17 00:16:50 +0800

    added some ui fixes

--- components/Lineup.tsx
+++ components/Lineup.tsx
@@ -4,9 +4,7 @@ import Image from 'next/image';
 import router from 'next/router';
 import Link from 'next/link';
 
-
 const Lineup = (props) => {
-
   const {
     position,
     player,
@@ -16,55 +14,63 @@ const Lineup = (props) => {
     score,
     teamName,
     nextposition,
-    onClick= null,
+    onClick = null,
     athleteLineup,
     index,
     test,
   } = props;
-  
-  function getPositionDisplay(position){
-    switch(position){
-      case 'QB' : return "QUARTER BACK";
-      case 'RB' : return "RUNNING BACK";
-      case 'WR' : return "WIDE RECEIVER";
-      case 'TE' : return "TIGHT END";
-      case 'FLEX' :
-      case 'SUPERFLEX': return position;
+
+  function getPositionDisplay(position) {
+    switch (position) {
+      case 'QB':
+        return 'QUARTER BACK';
+      case 'RB':
+        return 'RUNNING BACK';
+      case 'WR':
+        return 'WIDE RECEIVER';
+      case 'TE':
+        return 'TIGHT END';
+      case 'FLEX':
+      case 'SUPERFLEX':
+        return position;
     }
   }
   //const { position, player = '', img = null, id, score, nextposition, onClick = null } = props;
   const lineupPosition = '/images/tokensMLB/' + position + '.png';
   return (
     <>
-    
       <div className="flex justify-center cursor-pointer">
-      <Link href={{
-        pathname: '/AthleteSelect',
-        query: {
-          game_id: game_id,
-          position: test[0].position,
-          athleteLineup: JSON.stringify(test[1].lineup),
-          index: test[2].index,
-          teamName: teamName
-        },
-      }} as="/AthleteSelect">
-        {img ? (
-          <div className="relative mb-7" style={{ width: '120px', height: '162px' }}>
-            <div className="absolute z-40" style={{ width: '120px', height: '160px' }}></div>
-            <object
-              className="absolute z-10"
-              type="image/svg+xml"
-              data={img}
-              width={143}
-              height={190}
-            />
-            <div className="font-montserrat absolute z-50 text-sm top-1/3 left-4 text-indigo-white">{getPositionDisplay(position)}</div>
-          </div>
-        ) : (
-          <Image src={lineupPosition} width={143} height={190} />
-        )}
+        <Link
+          href={{
+            pathname: '/AthleteSelect',
+            query: {
+              game_id: game_id,
+              position: test[0].position,
+              athleteLineup: JSON.stringify(test[1].lineup),
+              index: test[2].index,
+              teamName: teamName,
+            },
+          }}
+          as="/AthleteSelect"
+        >
+          {img ? (
+            <div className="relative mb-7" style={{ width: '120px', height: '162px' }}>
+              <div className="absolute z-40" style={{ width: '120px', height: '160px' }}></div>
+              <object
+                className="absolute z-10"
+                type="image/svg+xml"
+                data={img}
+                width={143}
+                height={190}
+              />
+              <div className="font-montserrat absolute z-50 text-sm top-1/3 left-4 text-indigo-white">
+                {getPositionDisplay(position)}
+              </div>
+            </div>
+          ) : (
+            <Image src={lineupPosition} width={143} height={190} />
+          )}
         </Link>
-        
       </div>
       <div className="flex justify-center">
         <div className="flex flex-col mt-4">
