492a2a351eacae721d38280e5dad0debaf2f402b
Kendrell Derek Acibar <59480053+ciirno@users.noreply.github.com>
2022-11-23 18:23:07 +0800

    display leaderboards

--- pages/Games/[game_id].tsx
+++ pages/Games/[game_id].tsx
@@ -7,36 +7,114 @@ import { getContract, getRPCProvider } from 'utils/near';
 import { providers } from 'near-api-js';
 import { useWalletSelector } from 'contexts/WalletSelectorContext';
 import { useEffect, useState } from 'react';
-import { GAME } from 'data/constants/nearContracts';
-import { truncate } from 'utils/wallet';
+import { GAME,ATHLETE } from 'data/constants/nearContracts';
+import { getAthleteInfoById, convertNftToAthlete } from 'utils/athlete/helper';
 import Image from 'next/image';
-import PlayDetailsComponent from 'pages/PlayDetails/components/PlayDetailsComponent';
+import { useRouter } from 'next/router';
 import moment from 'moment';
 import Main from 'components/Main';
 
 const Games = (props) => {
   const { query } = props;
-  const [gameData, setGameData] = useState(null);
-
+  const router = useRouter();
+  const week = router.query.week;
   const gameId = query.game_id;
+
+  const [playerLineups, setPlayerLineups] = useState([]);
+
   const provider = new providers.JsonRpcProvider({
     url: getRPCProvider(),
   });
   const { accountId } = useWalletSelector();
   const [playerTeams, setPlayerTeams] = useState([]);
-  const [leaderboard, setLeaderboard] = useState([]);
 
-  function hasLeaderboard(start, end) {
-    const start_datetime = new Date(start);
-    const end_datetime = new Date(end);
-    const now = new Date();
+  function get_all_players_lineup() {
+    const query = JSON.stringify({
+      game_id: gameId,
+    });
 
-    if (now < start_datetime) {
-      return false;
-    } else {
-      return true;
-    }
+    provider
+      .query({
+        request_type: 'call_function',
+        finality: 'optimistic',
+        account_id: getContract(GAME),
+        method_name: 'get_all_players_lineup',
+        args_base64: Buffer.from(query).toString('base64'),
+      })
+      .then(async (data) => {
+        // @ts-ignore:next-line
+        const result = JSON.parse(Buffer.from(data.result).toString());
+
+        const arrayToReturn = await Promise.all(
+          result.map(async (item) => {
+            let itemToReturn = {
+              accountId: item[0][0],
+              teamName: item[0][2],
+              lineup: item[1].lineup,
+              sumScore: 0,
+            };
+
+            itemToReturn.lineup = await Promise.all(
+              itemToReturn.lineup.map((item) => {
+                return query_nft_token_by_id(item);
+              })
+            );
+
+            itemToReturn.lineup = itemToReturn.lineup.map((lineupItem) => {
+              return {
+                ...lineupItem,
+                stats_breakdown:
+                  lineupItem.stats_breakdown
+                    .filter(
+                      (statType) =>
+                        statType.type == 'weekly' && statType.played == 1 && statType.week == week
+                    )
+                    .map((item) => {
+                      return item.fantasyScore;
+                    })[0] || 0,
+              };
+            });
+
+            itemToReturn.sumScore = itemToReturn.lineup.reduce((accumulator, object) => {
+              return accumulator + object.stats_breakdown;
+            }, 0);
+
+            return itemToReturn;
+          })
+        );
+
+        arrayToReturn.sort(function (a, b) {
+          return b.sumScore - a.sumScore;
+        });
+
+        setPlayerLineups(arrayToReturn);
+      });
   }
+
+  function query_nft_token_by_id(item) {
+    const query = JSON.stringify({
+      token_id: item,
+    });
+
+    return provider
+      .query({
+        request_type: 'call_function',
+        finality: 'optimistic',
+        account_id: getContract(ATHLETE),
+        method_name: 'nft_token_by_id',
+        args_base64: Buffer.from(query).toString('base64'),
+      })
+      .then(async (data) => {
+        // @ts-ignore:next-line
+        const result = JSON.parse(Buffer.from(data.result).toString());
+        const result_two = await getAthleteInfoById(await convertNftToAthlete(result));
+        return result_two;
+      });
+  }
+
+  useEffect(() => {
+    get_all_players_lineup();
+  }, []);
 
   function query_player_teams() {
     const query = JSON.stringify({
@@ -67,129 +145,46 @@ const Games = (props) => {
   }, []);
 
   return (
-    <Container activeName="Games">
+    <Container activeName="GAMES">
       <div className="flex flex-row md:flex-col">
       <Main color="indigo-white">
         <div className="mt-8 ml-6">
           <BackFunction prev="/Play" />
         </div>
-        <>
           <div className="md:ml-6 mt-11 flex w-auto">
             <div className="md:ml-7 mr-12">
               <Image src="/images/game.png" width={550} height={279} />
             </div>
             <div className="md:ml-18 md:-mt-6 ml-18 -mt-6">
-              <ModalPortfolioContainer textcolor="indigo-black" title={'GAMES'} />
-              <div className="grid grid-cols-3 gap-4">
-                <div className="">
-                  <div>PRIZE POOL</div>
-                  <div className=" font-monument text-lg">
-                    {(gameData && gameData.prize) || 'N/A'}
-                  </div>
-                </div>
-                <div>
-                  <div>START DATE</div>
-                  <div className=" font-monument text-lg">
-                    {(gameData && moment(gameData.start_time).format('MM/DD/YYYY')) || 'N/A'}
-                  </div>
-                </div>
-              </div>
-              <div className="">
-                <div className="">
-                  {gameData &&
-                    (moment(gameData.start_time) <= moment() &&
-                    moment(gameData.end_time) > moment() ? (
-                      <>
-                        <p>ENDS IN</p>
-                        {gameData ? (
-                          <PlayDetailsComponent
-                            prizePool={gameData.prize}
-                            startDate={gameData.end_time}
-                            // fetch={() => fetchGameData()}
-                            // game={() => gameEnded()}
-                          />
-                        ) : (
-                          ''
-                        )}
-                      </>
-                    ) : moment(gameData.start_time) > moment() ? (
-                      <>
-                        <p>REGISTRATION ENDS IN</p>
-                        {gameData ? (
-                          <PlayDetailsComponent
-                            prizePool={gameData.prize}
-                            startDate={gameData.start_time}
-                            // fetch={() => fetchGameData()}
-                            // game={() => gameEnded()}
-                          />
-                        ) : (
-                          ''
-                        )}
-                      </>
-                    ) : (
-                      ''
-                    ))}
-                  <div className="flex justify-center md:justify-start w-3/5">
-                    <Link href={`/CreateLineup/${gameId}`}>
-                      <button className="bg-indigo-buttonblue text-indigo-white w-full h-12 text-center font-bold text-md mt-8">
-                        ENTER GAME
-                      </button>
-                    </Link>
-                  </div>
-                </div>
+              <ModalPortfolioContainer textcolor="indigo-black" title={'LEADERBOARD'} /> 
+              <div>
+                {playerLineups.length > 0
+                  ? playerLineups.map((item, index) => {
+                    return (
+                      <div className="flex flex-row" key={index}>
+                        <div
+                          className={`w-10 font-monument text-2xl ${
+                            index + 1 > 3 ? 'text-indigo-white' : ''
+                          }`}
+                        >
+                          {index + 1 <= 9 ? '0' + (index + 1) : index + 1}
+                        </div>
+                        <div className="ml-6 bg-indigo-black text-indigo-white
+                        w-full text-center p-1 text-base font-monument mb-5">
+                        {item.teamName}
+                        </div>
+                        <div className="ml-16 w-10 text-center font-black">{item.sumScore}</div>
+                      </div>
+                    );
+                  })
+                : ''}
               </div>
             </div>
-            {/* <div className="flex flex-col">
-              {hasLeaderboard('gameData.start_datetime', 'gameData.end_datetime') ? (
-                leaderboard.length > 0 ? (
-                  <>
-                    <PortfolioContainer textcolor="indigo-black mb-5" title="LEADERBOARD1" />
-                    {leaderboard.map(function (data, key) {
-                      return (
-                        <>
-                          <div className="ml-12 md:ml-10 mt-4 md:mt-5">
-                            <div className="flex text-center items-center">
-                              <div
-                                className={`w-10 mr-2 font-monument text-2xl ${
-                                  key + 1 > 3 ? 'text-indigo-white' : ''
-                                }`}
-                              >
-                                {key + 1 <= 9 ? '0' + (key + 1) : key + 1}
-                              </div>
-                              <div className="bg-indigo-black text-indigo-white px-3 text-center p-1 text-base font-monument">
-                                {truncate(data.player_addr, 11)}
-                              </div>
-                              <div className="ml-16 w-10 text-center font-black">
-                                {data.fantasy_score.toString().indexOf('.') === -1
-                                  ? `${data.fantasy_score}.00`
-                                  : data.fantasy_score}
-                              </div>
-                            </div>
-                          </div>
-                        </>
-                      );
-                    })}
-                  </>
-                ) : (
-                  <></>
-                )
-              ) : (
-                <>
-                  <PortfolioContainer textcolor="indigo-black" title="GAMEPLAY" />
-                  <div className="ml-7 mt-5 font-normal">
-                    Enter a team into the Alley-oop tournament to compete for cash prizes.
-                  </div>
-                  <div className="ml-7 mt-2 font-normal">
-                    Create a lineup by selecting five Playible Athlete Tokens now.
-                  </div>
-                </>
-              )}
-            </div> */}
           </div>
+
 
           <div className='mt-7 ml-6 w-3/5 md:w-1/3 md:ml-12 md:mt-2'>
           <ModalPortfolioContainer title="VIEW TEAMS" textcolor="text-indigo-black mb-5" />
-
           {
             /* @ts-expect-error */
             playerTeams.team_names == undefined ? (
@@ -222,7 +217,7 @@ const Games = (props) => {
             )
           }
           </div>
-        </>
+          
         </Main>
       </div>
     </Container>
