f3dcd625a2b50c99b96489a2847fbf1390fa8a85
Seaver Choy <matthew.seaver.choy@gmail.com>
2022-11-30 07:36:04 +0800

    Added custom fonts in head

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,8 +18,8 @@ import { AiOutlineVerticalRight, AiOutlineVerticalLeft } from 'react-icons/ai';
 import { GET_ATHLETES_TOP } from '../utils/queries';
 import { useLazyQuery, useQuery } from '@apollo/client';
 import Image from 'next/image';
-import {store} from 'redux/athlete/store';
-import {Provider} from 'react-redux';
+import { store } from 'redux/athlete/store';
+import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 let count = 0;
 
@@ -116,6 +116,12 @@ export default function Home(props) {
       <Head>
         <title>Playible - Next Generation of Sports Collectibles</title>
         <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;0,1000;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900;1,1000&display=swap"
+          rel="stylesheet"
+        />
         {/* <meta name="description" content="Playible - Next Generation of Sports Collectibles" />
       <meta name="keywords" content="fantasy, sports, draft, crypto, cryptocurrency, play"/>
 
