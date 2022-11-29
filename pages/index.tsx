6a7f48fab63b741a459dc009e484d3574b0ae095
Seaver Choy <matthew.seaver.choy@gmail.com>
2022-11-30 07:51:40 +0800

    added _document.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -113,23 +113,6 @@ export default function Home(props) {
 
   return (
     <Provider store={store}>
-      <Head>
-        <title>Playible - Next Generation of Sports Collectibles</title>
-        <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;0,1000;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900;1,1000&display=swap"
-          rel="stylesheet"
-        />
-        {/* <meta name="description" content="Playible - Next Generation of Sports Collectibles" />
-      <meta name="keywords" content="fantasy, sports, draft, crypto, cryptocurrency, play"/>
-
-      <meta property="og:title" content="Playible - Next Generation of Sports Collectibles"/>
-      <meta property="og:description" content="Playible - Next Generation of Sports Collectibles"/>
-      <meta property="og:url" content="https://playible.io/"/>
-      <meta property="og:type" content="website"/> */}
-      </Head>
       <Container activeName="HOME">
         <div className="flex flex-col w-screen md:w-full overflow-y-auto h-screen justify-center self-center md:pb-12 text-indigo-black">
           <Main color="indigo-white">
