71c5736d17f4f77d584db56bd7a75e934fc5b4eb
Ramon Arca <ramonteodoroarca@gmail.com>
2022-12-06 17:24:35 +0800

    Add button click handler to Claim Soulbound button

--- pages/Packs/index.tsx
+++ pages/Packs/index.tsx
@@ -183,7 +183,8 @@ export default function Packs() {
         <Main color="indigo-white">
           <div className="iphone5:mt-20 md:ml-6 md:mt-8">
             <button className="bg-indigo-buttonblue text-indigo-white w-5/6 md:w-80 h-10 
-              text-center font-bold text-xs self-center justify-center float-right md:mt-0 iphone5:mr-9 iphone5:mt-20">
+              text-center font-bold text-xs self-center justify-center float-right md:mt-0 iphone5:mr-9 iphone5:mt-20"
+              onClick={(e) => handleButtonClick(e)}>
               CLAIM SOULDBOUND PACK
             </button>
             <PortfolioContainer textcolor="indigo-black" title="PACKS">
