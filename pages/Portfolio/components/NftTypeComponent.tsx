e26559a34b88e880ee1db45e290f721706464f27
Kendrell Derek Acibar <59480053+ciirno@users.noreply.github.com>
2022-12-12 10:06:36 +0800

    Update NftTypeComponent

--- pages/Portfolio/components/NftTypeComponent.tsx
+++ pages/Portfolio/components/NftTypeComponent.tsx
@@ -2,23 +2,37 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 const NftTypeComponent = (props) => {
+  const [selected, setSelected] = useState(false);
+  const [selected2, setSelected2] = useState(false);
+
+  const handleChange = (event) => {
+    if (event.target.value === selected) {
+      setSelected(false);
+    } else if (event.target.value === selected2) {
+      setSelected2(false);
+    } else if (event.target.value === 'Regular') {
+      setSelected(event.target.value);
+    } else if (event.target.value === 'Soulbound') {
+      setSelected2(event.target.value);
+    }
+  };
+
   return (
     <form>
       <div className="flex flex-col float-right mr-60">
         NFT Type
         <div className="ml-9">
           <div>
-            <input type="checkbox" value="Regular"></input>
-            <label>Regular</label>
+            <input type="radio" value="Regular" checked={selected} onChange={handleChange} />
+            Regular
           </div>
           <div>
-            <input type="checkbox" value="Soulbound"></input>
-            <label>Soulbound</label>
+            <input type="radio" value="Soulbound" checked={selected2} onChange={handleChange} />
+            Soulbound
           </div>
         </div>
       </div>
     </form>
   );
 };
-
 export default NftTypeComponent;
