1e57d94ebdc059c1fca34867284de69b052e4b1e
Ramon Arca <ramonteodoroarca@gmail.com>
2022-12-23 19:47:54 +0800

    Create sportConstants file

---
+++ data/constants/sportConstants.ts
@@ -0,0 +1,16 @@
+export const NFL_POSITIONS = {
+  QB: 'QUARTER BACK',
+  RB: 'RUNNING BACK',
+  WR: 'WIDE RECEIVER',
+  TE: 'TIGHT END',
+}
+
+export const SPORT_TYPES = [
+  {
+    key: 'NFL',
+    sport: 'FOOTBALL',
+  },
+  {
+    key: 'NBA',
+    sport: 'BASKETBALL',
+  }];
