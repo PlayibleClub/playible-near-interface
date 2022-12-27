b5eb156be5e31d29ced7761ea0632bfc0248d6ad
Ramon Arca <ramonteodoroarca@gmail.com>
2022-12-27 21:09:34 +0800

    Update positions array in sportConstants

--- data/constants/sportConstants.ts
+++ data/constants/sportConstants.ts
@@ -1,11 +1,46 @@
 import { ATHLETE, ATHLETE_PROMO } from 'data/constants/nearContracts';
 import { getContract } from 'utils/near';
-export const NFL_POSITIONS = {
-  QB: 'QUARTER BACK',
-  RB: 'RUNNING BACK',
-  WR: 'WIDE RECEIVER',
-  TE: 'TIGHT END',
-}
+export const NFL_POSITIONS = [
+  {
+    name: 'QUARTER BACK',
+    key: 'QB',
+  },
+  {
+    name: 'RUNNING BACK',
+    key: 'RB',
+  },
+  {
+    name: 'WIDE RECEIVER',
+    key: 'WR',
+  },
+  {
+    name: 'TIGHT END',
+    key: 'TE',
+  },
+]
+export const NBA_POSITIONS = [
+  {
+    name: 'POINT GUARD',
+    key: 'PG',
+  },
+  {
+    name: 'SHOOTING GUARD',
+    key: 'SG',
+  },
+  {
+    name: 'SMALL FORWARD',
+    key: 'SF',
+  },
+  {
+    name: 'POWER FORWARD',
+    key: 'PF',
+  },
+  {
+    name: 'CENTER',
+    key: 'C',
+  }
+
+]
 
 export const SPORT_TYPES = [
   {
