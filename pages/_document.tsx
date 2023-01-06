1425cac527a33394df59c6a983e6f458950f39db
Seaver Choy <matthew.seaver.choy@gmail.com>
2023-01-06 15:10:44 +0800

    Change SEO from Sports Collectibles to Fantasy Sports

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,16 +16,16 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;0,1000;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900;1,1000&display=swap"
             rel="stylesheet"
           />
-          <title>Playible - Next Generation of Sports Collectibles</title>
+          <title>Playible - Next Generation of Fantasy Sports</title>
           <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png" />
 
-          <meta name="description" content="Playible - Next Generation of Sports Collectibles" />
+          <meta name="description" content="Playible - Next Generation of Fantasy Sports" />
           <meta name="keywords" content="fantasy, sports, draft, crypto, cryptocurrency, play" />
 
-          <meta property="og:title" content="Playible - Next Generation of Sports Collectibles" />
+          <meta property="og:title" content="Playible - Next Generation of Fantasy Sports" />
           <meta
             property="og:description"
-            content="Playible - Next Generation of Sports Collectibles"
+            content="Playible - Next Generation of Fantasy Sports"
           />
           <meta property="og:url" content="https://playible.io/" />
           <meta property="og:type" content="website" />
