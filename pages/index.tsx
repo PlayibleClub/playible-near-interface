7d156ecae2afdb48f9b0f4b92615ab392fe6f901
Kendrell Derek Acibar <59480053+ciirno@users.noreply.github.com>
2023-01-12 22:38:09 +0800

    Add new homepage desktop banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -98,7 +98,7 @@ export default function Home(props) {
 
   const [currentIndex, setCurrentIndex] = useState(0);
   const featuredImagesDesktop = [
-    '/images/basketball_starterpack_promotion_upcoming.png',
+    '/images/basketball_starterpack_publicmint.png',
     '/images/basketball_championship_ongoing.png',
   ];
 
