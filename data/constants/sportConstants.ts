f74e9a7604b9444d8f1fb19858f541e6c434a873
Seaver Choy <matthew.seaver.choy@gmail.com>
2023-01-12 18:14:57 +0800

    used constant instead

--- data/constants/sportConstants.ts
+++ data/constants/sportConstants.ts
@@ -1,6 +1,21 @@
-import { ATHLETE_NFL, ATHLETE_PROMO_NFL, GAME_NFL, MINTER_NFL, MINTER_BASKETBALL, PACK_NFL, PACK_PROMO_NFL, PACK_BASKETBALL, PACK_PROMO_BASKETBALL, ATHLETE_BASKETBALL, 
-  ATHLETE_PROMO_BASKETBALL, OPENPACK_NFL, OPENPACK_PROMO_NFL, OPENPACK_BASKETBALL, 
-  OPENPACK_PROMO_BASKETBALL, GAME_BASKETBALL} from 'data/constants/nearContracts';
+import {
+  ATHLETE_NFL,
+  ATHLETE_PROMO_NFL,
+  GAME_NFL,
+  MINTER_NFL,
+  MINTER_BASKETBALL,
+  PACK_NFL,
+  PACK_PROMO_NFL,
+  PACK_BASKETBALL,
+  PACK_PROMO_BASKETBALL,
+  ATHLETE_BASKETBALL,
+  ATHLETE_PROMO_BASKETBALL,
+  OPENPACK_NFL,
+  OPENPACK_PROMO_NFL,
+  OPENPACK_BASKETBALL,
+  OPENPACK_PROMO_BASKETBALL,
+  GAME_BASKETBALL,
+} from 'data/constants/nearContracts';
 import { getContract } from 'utils/near';
 export const NFL_POSITIONS = [
   {
@@ -19,7 +34,7 @@ export const NFL_POSITIONS = [
     name: 'TIGHT END',
     key: 'TE',
   },
-]
+];
 export const EXTRA_NFL = [
   {
     name: 'FLEX',
@@ -27,9 +42,9 @@ export const EXTRA_NFL = [
   },
   {
     name: 'SUPERFLEX',
-    key: ['QR', 'RB', 'WR', 'TE'], 
-  }
-]
+    key: ['QR', 'RB', 'WR', 'TE'],
+  },
+];
 export const NBA_POSITIONS = [
   {
     name: 'POINT GUARD',
@@ -50,8 +65,8 @@ export const NBA_POSITIONS = [
   {
     name: 'CENTER',
     key: 'C',
-  }
-]
+  },
+];
 export const EXTRA_NBA = [
   {
     name: 'GUARD',
@@ -64,21 +79,20 @@ export const EXTRA_NBA = [
   {
     name: 'ANY',
     key: ['PG', 'SG', 'SF', 'PF', 'C'],
-  }
-
-]
-export const SPORT_CONTRACT_LOOKUP= {
+  },
+];
+export const SPORT_CONTRACT_LOOKUP = {
   basketball: '.basketball.',
   football: '.nfl.',
-}
+};
 export const SPORT_NAME_LOOKUP = {
   basketball: 'BASKETBALL',
   football: 'FOOTBALL',
-}
+};
 export const SPORT_TYPES = [
   {
     key: 'NFL',
-    sport: 'FOOTBALL',
+    sport: SPORT_NAME_LOOKUP.football,
     mintContract: getContract(MINTER_NFL),
     packContract: getContract(PACK_NFL),
     packPromoContract: getContract(PACK_PROMO_NFL),
@@ -92,7 +106,7 @@ export const SPORT_TYPES = [
   },
   {
     key: 'NBA',
-    sport: 'BASKETBALL',
+    sport: SPORT_NAME_LOOKUP.basketball,
     mintContract: getContract(MINTER_BASKETBALL),
     packContract: getContract(PACK_BASKETBALL),
     packPromoContract: getContract(PACK_PROMO_BASKETBALL),
@@ -102,11 +116,12 @@ export const SPORT_TYPES = [
     openPromoContract: getContract(OPENPACK_PROMO_BASKETBALL),
     gameContract: getContract(GAME_BASKETBALL),
     positionList: NBA_POSITIONS,
-    extra: EXTRA_NBA
-  }];
+    extra: EXTRA_NBA,
+  },
+];
 
-  function getSportType(sport){
-    return SPORT_TYPES.find((x) => x.sport === sport);
-  }
+function getSportType(sport) {
+  return SPORT_TYPES.find((x) => x.sport === sport);
+}
 
-  export { getSportType };
+export { getSportType };
