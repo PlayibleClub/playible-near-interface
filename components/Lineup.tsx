a67879a500face2ffff675f2bb46c54406ba1e1a
chanXDD <75827562+chanXDD@users.noreply.github.com>
2023-02-03 00:48:53 +0800

    create team mobile view wip

--- components/Lineup.tsx
+++ components/Lineup.tsx
@@ -31,13 +31,13 @@ const Lineup = (props) => {
           <div className="justify-center relative mb-7" style={{ width: '120px', height: '162px' }}>
             <div className="absolute z-40" style={{ width: '120px', height: '160px' }}></div>
             <object
-              className="absolute z-10"
+              className="absolute z-10 transform scale-55 md:scale-100"
               type="image/svg+xml"
               data={img}
               width={143}
               height={190}
             />
-            <div className="w-24 ml-1 text-center font-montserrat absolute z-50 text-sm top-1/3 left-4 text-indigo-white">
+            <div className="w-24 ml-1 text-center font-montserrat absolute z-50 text-sm top-1/3 left-4 text-indigo-white transform scale-55 md:scale-100">
               {isAthlete ? '' : getPositionDisplay(position, currentSport)}
             </div>
           </div>
@@ -47,10 +47,10 @@ const Lineup = (props) => {
         {/* </Link> */}
       </div>
       <div className="flex justify-center">
-        <div className="flex flex-col mt-4">
-          <div className="mb-3 text-sm uppercase font-bold">{player === '' ? '-' : player}</div>
-          <div className="text-xs font-thin">FANTASY SCORE</div>
-          <div className="text-xs font-bold">{score === '' ? '-' : score}</div>
+        <div className="flex flex-col ml-6 -mt-8 md:mt-0">
+          <div className="mb-3 text-sm uppercase font-bold transform scale-65 md:scale-100">{player === '' ? '-' : player}</div>
+          <div className="text-xs font-thin transform scale-65 md:scale-100 -mt-4">FANTASY SCORE</div>
+          <div className="text-xs font-bold transform scale-65 md:scale-100">{score === '' ? '-' : score}</div>
         </div>
       </div>
     </>
--- pages/CreateTeam/[sport]/[game_id].tsx
+++ pages/CreateTeam/[sport]/[game_id].tsx
@@ -525,11 +525,11 @@ export default function CreateLineup(props) {
                   ''
                 )}
                 <div className={`${selectModal ? 'hidden h-0' : ''}`}>
-                  <div className="md:ml-6">
+                  <div className="md:ml-6 mt-10">
                     <PortfolioContainer title="CREATE LINEUP" textcolor="text-indigo-black" />
                   </div>
                   <div className="flex flex-col -mt-8 -mb-5">
-                    <div className="flex items-end pt-10 pb-3 ml-7">
+                    <div className="flex items-end pt-10 pb-3 ml-7 -mt-8">
                       <div className="font-monument text-xl ml-6 truncate w-40 md:w-min md:max-w-xs">
                         {teamName}
                       </div>
@@ -540,7 +540,7 @@ export default function CreateLineup(props) {
                         EDIT TEAM NAME
                       </p>
                     </div>
-                    <div className="grid grid-cols-2 gap-y-4 mt-2 mb-2 md:mb-10 md:grid-cols-4 md:ml-7 md:mt-12">
+                    <div className="grid grid-cols-4  md:gap-y-4 -mt-12 mb-2 md:mb-10 md:grid-cols-4 md:ml-7 md:mt-12 -ml-6 mr-6 md:mr-0 ">
                       {lineup.map((data, i) => {
                         console.log(lineup);
                         return (
@@ -591,7 +591,7 @@ export default function CreateLineup(props) {
                       })}
                     </div>
                   </div>
-                  <div className="flex bg-indigo-black bg-opacity-5 w-full justify-end">
+                  <div className="flex bg-indigo-black bg-opacity-5 w-full justify-end fixed bottom-0">
                     <button
                       className="bg-indigo-buttonblue text-indigo-white w-full md:w-80 h-14 text-center font-bold text-md"
                       onClick={() => setSubmitModal(true)}
