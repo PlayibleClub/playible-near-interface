530f979e05a3e0c17c53a935f490548be68b2f73
Seaver Choy <matthew.seaver.choy@gmail.com>
2023-02-05 14:56:30 +0800

    Updated homepage image

--- pages/index.tsx
+++ pages/index.tsx
@@ -160,12 +160,12 @@ export default function Home(props) {
 
   const [currentIndex, setCurrentIndex] = useState(0);
   const featuredImagesDesktop = [
-    '/images/basketball_starterpack_publicmint.png',
+    '/images/basketball_starterpack_promotion_desktop.png',
     '/images/basketball_championship_ongoing.png',
   ];
 
   const featuredImagesMobile = [
-    '/images/basketball_starterpack_promotion_upcoming.png',
+    '/images/basketball_starterpack_publicmint_mobile.png',
     '/images/basketball_championship_ongoing.png',
   ];
 
