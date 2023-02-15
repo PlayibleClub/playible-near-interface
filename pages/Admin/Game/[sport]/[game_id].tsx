ce716d26d473acc01c59f6fb88fcdae856cfe4fb
Kendrell Derek Acibar <59480053+ciirno@users.noreply.github.com>
2023-02-15 11:09:12 +0800

    Update admin game to uses filter keys query

--- pages/Admin/Game/[sport]/[game_id].tsx
+++ pages/Admin/Game/[sport]/[game_id].tsx
@@ -3,7 +3,12 @@ import { useRouter } from 'next/router';
 import { getContract, getRPCProvider } from 'utils/near';
 import { providers } from 'near-api-js';
 import { getAthleteInfoById, convertNftToAthlete } from 'utils/athlete/helper';
-import { query_all_players_lineup, query_game_data } from 'utils/near/helper';
+import {
+  compute_scores,
+  query_all_players_lineup,
+  query_game_data,
+  query_player_lineup,
+} from 'utils/near/helper';
 import { getNflWeek, getNflSeason, formatToUTCDate } from 'utils/date/helper';
 import { getSportType, SPORT_TYPES, SPORT_NAME_LOOKUP } from 'data/constants/sportConstants';
 import moment, { Moment } from 'moment';
@@ -25,22 +30,57 @@ export default function AdminPlayerLineup(props) {
     url: getRPCProvider(),
   });
 
-  async function get_all_players_lineup() {
-    let gameData = await query_game_data(gameId, getSportType(currentSport).gameContract);
-    // setNflSeason(await getNflSeason(gameData.start_time / 1000));
-    // setWeek(await getNflWeek(gameData.start_time / 1000));
-    // console.log(nflSeason);
+  async function get_all_player_keys() {
+    const query = JSON.stringify({});
+    return await provider
+      .query({
+        request_type: 'call_function',
+        finality: 'optimistic',
+        account_id: getSportType(currentSport).gameContract,
+        method_name: 'get_player_lineup_keys',
+        args_base64: Buffer.from(query).toString('base64'),
+      })
+      .then(async (data) => {
+        //@ts-ignore:next-line
+        const result = JSON.parse(Buffer.from(data.result).toString());
+        //console.log(result);
+        return result;
+      });
+  }
 
+  async function get_all_players_lineup_with_index() {
+    let gameData = await query_game_data(gameId, getSportType(currentSport).gameContract);
     const startTimeFormatted = formatToUTCDate(gameData.start_time);
     const endTimeFormatted = formatToUTCDate(gameData.end_time);
+    // console.log('    TEST start date: ' + startTimeFormatted);
+    // console.log('    TEST end date: ' + endTimeFormatted);
 
-    setPlayerLineups(
-      await query_all_players_lineup(gameId, currentSport, startTimeFormatted, endTimeFormatted)
-    );
+    await get_all_player_keys().then(async (result) => {
+      let filteredResult = result.filter((data) => data[1] === gameId);
+      //console.log(filteredResult);
+      let lineups = [];
+
+      for (const entry of filteredResult) {
+        await query_player_lineup(currentSport, entry[0], entry[1], entry[2]).then((lineup) => {
+          if (lineups.length === 0) {
+            lineups = [lineup];
+          } else {
+            lineups = lineups.concat([lineup]);
+          }
+        });
+      }
+      let computedLineup = await compute_scores(
+        lineups,
+        currentSport,
+        startTimeFormatted,
+        endTimeFormatted
+      );
+      setPlayerLineups(computedLineup);
+    });
   }
 
   useEffect(() => {
-    get_all_players_lineup();
+    get_all_players_lineup_with_index();
   }, [currentSport, week, nflSeason]);
 
   return (
