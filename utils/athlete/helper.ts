3334812657e738b422d219e9e491e6eb3175622a
Ramon Arca <ramonteodoroarca@gmail.com>
2023-02-22 16:03:01 +0800

    Add helper function to get nba player schedule
    Based on the date provided

--- utils/athlete/helper.ts
+++ utils/athlete/helper.ts
@@ -1,8 +1,10 @@
 import client from 'apollo-client';
 import { objectTraps } from 'immer/dist/internal';
-import { GET_ATHLETE_BY_ID, GET_ATHLETE_BY_ID_DATE } from '../queries';
-import { getUTCTimestampFromLocal } from 'utils/date/helper';
+import { GET_ATHLETE_BY_ID, GET_ATHLETE_BY_ID_DATE, GET_NBA_PLAYER_SCHEDULE } from '../queries';
+import { formatToUTCDate, getUTCTimestampFromLocal } from 'utils/date/helper';
 import { getSportType } from 'data/constants/sportConstants';
+import { useSelector } from 'react-redux';
+
 // pull from graphQL and append the nft animation
 // return assembled Athlete
 async function getAthleteInfoById(item) {
@@ -35,7 +37,62 @@ async function getAthleteInfoById(item) {
   };
   return returningData;
 }
+async function getAthleteBasketballSchedule(athlete, startDate, endDate){
+  
 
+  const { data }  = await client.query({
+    query: GET_NBA_PLAYER_SCHEDULE,
+    variables: {
+      team: athlete.team,
+      startDate: formatToUTCDate(startDate),//formatToUTCDate(1676418043000),
+      endDate: formatToUTCDate(endDate),//formatToUTCDate(1677282043000),
+    }
+  });
+
+  return {...athlete, schedule: data.getNbaPlayerSchedule};
+}
+// async function getAthleteInfoBasketballSelect(item){
+//   let value = item.extra.map((item) => item.value);
+//   let startDate = useSelector(getGameStartDate);
+//   let endDate = useSelector(getGameEndDate);
+//   const { data } = await client.query({
+//     query: GET_ATHLETE_BY_ID,
+//     variables: { getAthleteById: parseFloat(value[0]) },
+//   });
+//   const basketball = item.token_id.includes('2000');
+//   const diff = item.token_id.includes('SB') || item.token_id.includes('PR') ? 1 : basketball ? 1 : 0;
+//   const isPromo = item.token_id.includes('SB') || item.token_id.includes('PR');
+//   const teamKey = value[4 - diff];
+//   const schedule = await client.query({
+//     query: GET_NBA_PLAYER_SCHEDULE,
+//     variables: { 
+//       team: teamKey, 
+//       startDate: formatToUTCDate(startDate), 
+//       endDate: formatToUTCDate(endDate)
+//     }
+//   })
+//   console.log(schedule.data);
+//   const returningData = {
+//     primary_id: value[0],
+//     athlete_id: item.token_id,
+//     rarity: value[1],
+//     usage: isPromo ? 0 : basketball ? 0 : value[2],
+//     name: value[3 - diff],
+//     team: value[4 - diff],
+//     position: value[5 - diff],
+//     release: value[6 - diff],
+//     ...(isPromo && { type: value[7 - diff] }),
+//     isOpen: false,
+//     animation: data.getAthleteById.nftAnimation,
+//     image: item.metadata.media,
+//     fantasy_score: getAvgFantasyScore(data.getAthleteById.stats),
+//     stats_breakdown: data.getAthleteById.stats,
+//     isInGame: item.metadata['starts_at'] > getUTCTimestampFromLocal() ? true : false,
+//     isInjured: data.getAthleteById.isInjured,
+//     isActive: data.getAthleteById.isActive,
+//   };
+//   return returningData;
+// }
 async function getAthleteInfoByIdWithDate(item, from, to) {
   let value = item.extra.map((item) => item.value);
   const { data } = await client.query({
@@ -142,4 +199,4 @@ function cutAthleteName(name){
   return newName;
 }
 
-export { convertNftToAthlete, getAthleteInfoById, getAthleteInfoByIdWithDate, getPositionDisplay, checkInjury, cutAthleteName};
+export { convertNftToAthlete, getAthleteInfoById, getAthleteBasketballSchedule, getAthleteInfoByIdWithDate, getPositionDisplay, checkInjury, cutAthleteName};
