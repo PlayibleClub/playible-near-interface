bbd3ebbdc03bd9f19a70729136f127a0f1570b6c
Ramon Arca <ramonteodoroarca@gmail.com>
2023-03-06 12:43:41 +0800

    Remove commented function

--- utils/athlete/helper.ts
+++ utils/athlete/helper.ts
@@ -67,48 +67,6 @@ async function getAthleteBasketballSchedule(athlete, startDate, endDate){
 
   return {...athlete, schedule: data.getNbaPlayerSchedule};
 }
-// async function getAthleteInfoBasketballSelect(item){
-//   let value = item.extra.map((item) => item.value);
-//   let startDate = useSelector(getGameStartDate);
-//   let endDate = useSelector(getGameEndDate);
-//   const { data } = await client.query({
-//     query: GET_ATHLETE_BY_ID,
-//     variables: { getAthleteById: parseFloat(value[0]) },
-//   });
-//   const basketball = item.token_id.includes('2000');
-//   const diff = item.token_id.includes('SB') || item.token_id.includes('PR') ? 1 : basketball ? 1 : 0;
-//   const isPromo = item.token_id.includes('SB') || item.token_id.includes('PR');
-//   const teamKey = value[4 - diff];
-//   const schedule = await client.query({
-//     query: GET_NBA_PLAYER_SCHEDULE,
-//     variables: { 
-//       team: teamKey, 
-//       startDate: formatToUTCDate(startDate), 
-//       endDate: formatToUTCDate(endDate)
-//     }
-//   })
-//   console.log(schedule.data);
-//   const returningData = {
-//     primary_id: value[0],
-//     athlete_id: item.token_id,
-//     rarity: value[1],
-//     usage: isPromo ? 0 : basketball ? 0 : value[2],
-//     name: value[3 - diff],
-//     team: value[4 - diff],
-//     position: value[5 - diff],
-//     release: value[6 - diff],
-//     ...(isPromo && { type: value[7 - diff] }),
-//     isOpen: false,
-//     animation: data.getAthleteById.nftAnimation,
-//     image: item.metadata.media,
-//     fantasy_score: getAvgFantasyScore(data.getAthleteById.stats),
-//     stats_breakdown: data.getAthleteById.stats,
-//     isInGame: item.metadata['starts_at'] > getUTCTimestampFromLocal() ? true : false,
-//     isInjured: data.getAthleteById.isInjured,
-//     isActive: data.getAthleteById.isActive,
-//   };
-//   return returningData;
-// }
 async function getAthleteInfoByIdWithDate(item, from, to) {
   let value = item.extra.map((item) => item.value);
   const { data } = await client.query({
