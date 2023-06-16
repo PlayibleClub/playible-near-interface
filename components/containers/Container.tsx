f07db083f44658afb5f7a59e3e3d6a076144ba62
Lilith <nekopass.420loli@gmail.com>
2023-06-16 18:10:46 +0800

    Update Container.tsx

--- components/containers/Container.tsx
+++ components/containers/Container.tsx
@@ -17,9 +17,31 @@ const Container = (props) => {
   return (
     <div className="font-montserrat h-min md:h-screen relative hide-scroll bg-indigo-white flex overflow-x-hidden overflow-y-hidden">
       <Head>
+
+        <script
+          type="text/javascript"
+          async
+          dangerouslySetInnerHTML={{
+            __html: `
+          (function(w, d, s, l, i) {
+                w[l] = w[l] || []
+                w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" })
+                const f = d.getElementsByTagName(s)[0],
+                  j = d.createElement(s),
+                  dl = l != "dataLayer" ? "&l=" + l : ""
+                j.async = true
+                j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl
+                f.parentNode.insertBefore(j, f)
+              })(window,document,'script','dataLayer',"GTM-KQBL75W")`,
+          }}/>
+
         <title>Playible - Next Generation of Fantasy Sports</title>
         <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png" />
       </Head>
+
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KQBL75W"
+      height="0" width="0" className="hidden"></iframe></noscript>
+
       <div className="invisible w-0 md:visible md:w-full">
         <div className="flex bg-indigo-white">
           <DesktopNavbar
