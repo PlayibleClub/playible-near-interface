2e0e8378eecd85bab80a46ff2afce12ea4151ad6
SleepyBao <108985842+SleepyBao@users.noreply.github.com>
2023-09-01 19:01:14 +0800

    football headdshots

--- pages/AssetDetails/[sport]/[id].tsx
+++ pages/AssetDetails/[sport]/[id].tsx
@@ -215,7 +215,7 @@ const AssetDetails = (props) => {
           </div>
         </div>
 
-        {currentSport === 'BASEBALL' ? (
+        {currentSport === 'BASEBALL' || currentSport === 'FOOTBALL' ? (
           <div className="iphone5:mt-2 md:mt-5">
             <div
               className="bg-no-repeat iphone5:bg-cover bg-auto md:bg-contain md:bg-[length:1400px_300px] lg:bg-auto"
