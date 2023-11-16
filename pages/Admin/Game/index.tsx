0e9acbe2ae957a80bb508d4ac019ee31dcfb004f
SleepyBao <108985842+SleepyBao@users.noreply.github.com>
2023-11-16 23:01:23 +0800

    fixed console.logs for whitelist

--- pages/Admin/Game/index.tsx
+++ pages/Admin/Game/index.tsx
@@ -854,7 +854,7 @@ export default function Index(props) {
         game_id: details.gameId.toString(),
         game_time_start: dateStart,
         game_time_end: dateEnd,
-        tokenTypeWhitelist: whitelistInfo,
+        whitelist: whitelistInfo,
         token_type_whitelist: tokenTypeWhitelist,
         //token_type_whitelist: ['regular', 'soulbound', 'promo'],
         positions:
