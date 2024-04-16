ccf2fcb63435e78195a9452096c35dc5b93f6cbd
Ramon Arca <ramonteodoroarca@gmail.com>
2024-04-16 10:11:34 +0800

    Fix Back button in AthleteSelect forcing a refresh

--- components/buttons/BackFunction.tsx
+++ components/buttons/BackFunction.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { BrowserView } from 'react-device-detect';
+import Link from 'next/link';
 
 const BackFunction = (props) => {
   const { prev } = props;
   return (
     <BrowserView>
       <div className="ml-7 text-indigo-buttonblue flex flex-row">
-        <a href={`${prev}`} className="flex flex-row">
+        <Link href={`${prev}`} className="flex flex-row">
           <img src="/images/navicons/icon_back.png" />
           <div className="mt-0.25">BACK</div>
-        </a>
+        </Link>
       </div>
     </BrowserView>
   );
